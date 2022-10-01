45727d84316bc68591aee9236525429571ebd8de
LEo _ <Lev Bespalov>
2022-10-01 22:01:58 +0300

    Bring parts to the ship to get scores

--- Assets/Scripts/GameRoot.cs
+++ Assets/Scripts/GameRoot.cs
@@ -6,7 +6,7 @@ public class GameRoot : Root
 
     [SerializeField] private PlayerMovement _playerMovement;
     [SerializeField] private ShipMovement _shipMovement;
-    [SerializeField] private PartsHolder _partsHolder;
+    [SerializeField] private PartsSpawner partsSpawner;
     [SerializeField] private ShipExplosionListener _shipExplosionListener;
     
     [SerializeField] private Bounds _bounds;
@@ -18,7 +18,7 @@ public class GameRoot : Root
         new PlayerInExplosionChecker(blast, _playerMovement, GameInstance);
         
         _shipMovement.Init(blast);
-        _partsHolder.Init(blast);
+        partsSpawner.Init(blast);
         _shipExplosionListener.Init(blast);
         // _mapSpawner.Init(_shipMovement);
     }
---
+++ Assets/Scripts/PutPartsToTheShipTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PutPartsToTheShipTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ScoresHolder.ApplyScores();
+        }
+    }
+}
