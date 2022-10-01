c2f47c824444a8e7030133fcc3acbad2ea0b7231
LEo _ <Lev Bespalov>
2022-10-01 12:17:43 +0300

    fastfix

--- Assets/Scripts/Blast.cs
+++ Assets/Scripts/Blast.cs
@@ -6,7 +6,7 @@ public class Blast
     public event Action Explosion;
 
     private readonly float _timeBetweenBlasts;
-    public float BlastTimer { get; private set; };
+    public float BlastTimer { get; private set; }
     public Vector3 Position { get; private set; } = new Vector3(0, 0,-0.1f);
 
     public Blast(float blastTimer = 10f)
