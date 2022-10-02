84c239bb9e1278ace9b07d2011fb2d7531d83225
Felor <antti.hamara@gmail.com>
2022-10-02 16:27:37 +0300

    Zoom delay to camera

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public float MaxSize = 14f;
+    public float MinSize = 10f;
     public float DelayFactor = 0.1f;
     private GameObject playerObj = null;
 
@@ -18,11 +20,16 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        float currentSize = GetComponent<Camera>().orthographicSize;
         Vector3 cameraPos = gameObject.transform.position;
         Vector3 playerPos = playerObj.transform.position;
+        float distance = Vector3.Distance(cameraPos, playerPos) - 8f;
+        Debug.Log(distance);
         cameraPos = Vector3.Lerp(cameraPos, playerPos, DelayFactor*Time.deltaTime);
         cameraPos.y = 8;
+
+        float sizeTagget = Mathf.Lerp(MinSize, MaxSize, distance);
+        GetComponent<Camera>().orthographicSize = Mathf.Lerp(currentSize, sizeTagget, DelayFactor * Time.deltaTime);
 
         gameObject.transform.position = cameraPos;
     }
