829502aa6e790151cef0374f4ea2fb0a9c635a62
Felor <antti.hamara@gmail.com>
2022-10-02 16:07:59 +0300

    Smoother Camera follow

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -16,7 +16,7 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
         Vector3 cameraPos = gameObject.transform.position;
